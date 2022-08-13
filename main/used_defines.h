fce6d79ba5d14ec87c78a5add791f15d16cf97e9
palee96 <paleeman96@gmail.com>
2022-08-13 12:52:02 +0200

    Added Wifimanager-palee,multiple source files, etc

--- main/used_defines.h
+++ main/used_defines.h
@@ -1,7 +1,7 @@
 #define esp_wifi_id "testAP"  
 #define esp_wifi_pass "testAP69"
 
-#define BROKER_URL "mqtt://palee:aio_pKzO23X1M877s5nDfmUotznBSCK4@io.adafruit.com"
+#define BROKER_URL ""
 #define IO_TOPIC "palee/feeds/jsonstuff"
 
 #define maximum_retries 3
